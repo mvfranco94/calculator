d3232a89e1953b94baeb965a40af1197a29a3f78
Marcos Franco <156917786+mvfranco94@users.noreply.github.com>
2025-09-29 11:42:25 -0300

    Add basic math operation's functions

---
+++ app.js
@@ -0,0 +1,9 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+console.log(`add(2,2): ${add(2, 2)}`);
+console.log(`subtract(2,2): ${subtract(2, 2)}`);
+console.log(`multiply(2,2): ${multiply(2, 2)}`);
+console.log(`divide(2,2): ${divide(2, 2)}`);
