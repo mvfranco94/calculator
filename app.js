e9f4a4c1063dcd6ff73f3c8a58c733368ad50725
Marcos Franco <156917786+mvfranco94@users.noreply.github.com>
2025-09-29 12:07:15 -0300

    Add operate function
    Operate fuction will control which operation need
    to be done.

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
+let number1 = 0;
+let number2 = 0;
+let operator = "";
+
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-console.log(`add(2,2): ${add(2, 2)}`);
-console.log(`subtract(2,2): ${subtract(2, 2)}`);
-console.log(`multiply(2,2): ${multiply(2, 2)}`);
-console.log(`divide(2,2): ${divide(2, 2)}`);
+function operate(operator, number1, number2) {
+  switch (operator) {
+    case "+":
+      return add(number1, number2);
+    case "-":
+      return subtract(number1, number2);
+    case "*":
+      return multiply(number1, number2);
+    case "/":
+      return divide(number1, number2);
+  }
+}
+
+number1 = 3;
+number2 = 2;
+
+operator = "+"
+console.log(`operate ${number1} ${operator} ${number2} : `, operate(operator, number1, number2));
+
+operator = "-"
+console.log(`operate ${number1} ${operator} ${number2} : `, operate(operator, number1, number2));
+
+operator = "*"
+console.log(`operate ${number1} ${operator} ${number2} : `, operate(operator, number1, number2));
+
+operator = "/"
+console.log(`operate ${number1} ${operator} ${number2} : `, operate(operator, number1, number2));
