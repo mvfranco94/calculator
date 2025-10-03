796f2ff3d051bd2c5e726882bbec4b1144e7edb9
Marcos Franco <156917786+mvfranco94@users.noreply.github.com>
2025-10-03 20:09:27 -0300

    Add populate function
    When the user click on numbers and operators the
    display is populate.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const container = document.querySelector("#container");
 const display = document.querySelector("#display");
 
-let number1 = 0;
-let number2 = 0;
+let number1 = null;
+let number2 = null;
 let operator = null;
 
 const add = (a, b) => a + b;
@@ -19,8 +19,13 @@ container.addEventListener("click", function (e) {
 
   if (target.value === "clear") {
     display.value = "";
+    number1 = null;
+    number2 = null;
+    operator = null;
     return;
   }
+
+  populateDisplay(target.value);
 })
 
 function operate(operator, number1, number2) {
@@ -34,4 +39,22 @@ function operate(operator, number1, number2) {
     case "/":
       return divide(number1, number2);
   }
+}
+
+function populateDisplay(value) {
+  if (!number1 && !isNaN(value)) {
+    number1 = value;
+  }
+
+  if (!operator && "+-*/".includes(value) && number1 !== null) {
+    operator = value;
+  }
+
+  if (!number2 && !isNaN(value) && number1 !== null && operator !== null) {
+    number2 = value;
+  }
+
+  display.value = number1 ? number1 : "";
+  display.value += operator ? operator : "";
+  display.value += number2 ? number2 : "";
 }
